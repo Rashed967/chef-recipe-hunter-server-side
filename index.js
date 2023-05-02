84ade9c35fac394ac23503dda76a9aa5134d0a18
Rashed967 <rashedhasan967.com>
2023-05-02 09:45:48 +0600

    loader chefs done

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+var cors = require('cors')
+
+app.use(cors())
+
+const navBar = require('./data/nav.json')
+const chefs = require('./data/chefs.json')
+
+
 app.get('/', (req, res) => {
     res.send("Hello world")
+})
+
+app.get('/nav', (req, res) => {
+    res.send(navBar)
+})
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs)
 })
 
 app.listen(port , () => {
